13b39aff595606fe1eed4127e152f503cc3a3189
Will <willyderscanu@yahoo.com>
2019-03-25 15:06:52 +0200

    transported logic from old project

--- Data Structures and Algorithms/List.h
+++ Data Structures and Algorithms/List.h
@@ -8,14 +8,14 @@ class List
 public:
 	List();
 
-	void push_back(DT data)
+	void push_back(DT data);
 	void pop_back();
 
 	void add_front(DT data);
 	void del_front();
 
-	void erase(T data, uint64_t pos);
-	void insert(T data, uint64_t pos)
+	void erase(DT data, uint64_t pos);
+	void insert(DT data, uint64_t pos);
 
 	DT front() const;
 	DT back() const;
@@ -23,8 +23,8 @@ public:
 	uint64_t getSize() const;
 	uint64_t getCapacity() const;
 
-	T& operator[](uint64_t index);
-	T operator[](uint64_t index) const
+	DT& operator[](uint64_t index);
+	DT operator[](uint64_t index) const;
 	
 	~List();
 private:
@@ -32,7 +32,7 @@ private:
 	bool isFull() const;
 	void Resize();
 private:
-	friend std::ostream& operator<<(std::ostream& out, const List<T>& lst);
+	friend std::ostream& operator<<(std::ostream& out, const List<DT>& lst);
 private:
 	static constexpr uint8_t Start_size = 10;
 	static constexpr uint8_t Increment = 5;
@@ -42,3 +42,133 @@ private:
 
 	DT* pVect;
 };
+
+template<typename DT>
+inline List<DT>::List()
+	:
+	pVect(new DT[capacity])
+{
+}
+
+template<typename DT>
+inline void List<DT>::push_back(DT data)
+{
+	if (isFull())
+		Resize();
+	pVect[size++] = data;
+}
+
+template<typename DT>
+inline void List<DT>::pop_back()
+{
+	if (!isEmpty())
+		size--;
+}
+
+template<typename DT>
+inline void List<DT>::add_front(DT data)
+{
+	if (isFull())
+		Resize();
+	for (uint64_t i = size; i > 0; i--)
+		pVect[i] = pVect[i - 1];
+
+	pVect[0] = data;
+	size++;
+}
+
+template<typename DT>
+inline void List<DT>::del_front()
+{
+	for (uint64_t i = 0; i < size - 1; i++)
+		pVect[i] = pVect[i + 1];
+	size--;
+}
+
+template<typename DT>
+inline void List<DT>::erase(DT data, uint64_t pos)
+{
+	for (uint64_t i = pos; i < size - 1; i++)
+		pVect[i] = pVect[i + 1];
+	size--;
+}
+
+template<typename DT>
+inline void List<DT>::insert(DT data, uint64_t pos)
+{
+	if (isFull())
+		Resize();
+	for (uint64_t i = size; i > pos; i--)
+		pVect[i] = pVect[i - 1];
+	pVect[pos] = data;
+	size++;
+}
+
+template<typename DT>
+inline DT List<DT>::front() const
+{
+	if (isEmpty())
+		throw std::out_of_range("no element in the list");
+	return pVect[0];
+}
+
+template<typename DT>
+inline DT List<DT>::back() const
+{
+	if (isEmpty())
+		throw std::out_of_range("no element in the list");
+	return pVect[size - 1];
+}
+
+template<typename DT>
+inline uint64_t List<DT>::getSize() const
+{
+	return size;
+}
+
+template<typename DT>
+inline uint64_t List<DT>::getCapacity() const
+{
+	return capacity;
+}
+
+template<typename DT>
+inline DT & List<DT>::operator[](uint64_t index)
+{
+	return pVect[index];
+}
+
+template<typename DT>
+inline DT List<DT>::operator[](uint64_t index) const
+{
+	return pVect[index];
+}
+
+template<typename DT>
+inline List<DT>::~List()
+{
+	delete[] pVect;
+	pVect = nullptr;
+}
+
+template<typename DT>
+inline bool List<DT>::isEmpty() const
+{
+	return size == 0;
+}
+
+template<typename DT>
+inline bool List<DT>::isFull() const
+{
+	return size == capacity;
+}
+
+template<typename DT>
+inline void List<DT>::Resize()
+{
+}
+
+template<typename DT>
+inline std::ostream& operator<<(std::ostream& out, const List<DT>& lst)
+{
+}
